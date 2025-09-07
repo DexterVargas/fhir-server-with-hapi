dfbe2922cbd7841d4d4f3267e4eeca53a104e66e
dextervargasDev <dextervargas0912@gmail.com>
2025-09-07 08:00:23 +0800

    first commit

---
+++ src/main/java/com/dexterv/fhirserverwithhapi/FhirServerWithHapiApplication.java
@@ -0,0 +1,13 @@
+package com.dexterv.fhirserverwithhapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirServerWithHapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhirServerWithHapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dexterv/fhirserverwithhapi/FhirServerWithHapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexterv.fhirserverwithhapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirServerWithHapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
